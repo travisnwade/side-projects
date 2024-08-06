f2d69dd960fb6ad7d5778afd59dd1aa656dd7d0a
Travis Wade <travis@twade.io>
2024-08-06 13:03:50 -0400

    few updated changes

--- html-fun/random-ducks/game/assets/bg-music-volume.js
+++ html-fun/random-ducks/game/assets/bg-music-volume.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const bgMusic = document.getElementById('bgMusic');
     const volumeSlider = document.getElementById('volumeSlider');
 
-    volumeSlider.addEventListener('input', function() {
+    function updateVolume() {
         bgMusic.volume = volumeSlider.value;
-    });
+        volumeSlider.style.setProperty('--volume', volumeSlider.value);
+    }
+
+    volumeSlider.addEventListener('input', updateVolume);
 
     // Set initial volume
-    bgMusic.volume = volumeSlider.value = 0.25;
+    bgMusic.volume = volumeSlider.value = 0.5;
+    updateVolume();
 });
